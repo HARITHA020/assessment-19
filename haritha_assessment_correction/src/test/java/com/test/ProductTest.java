8bf9971ba926c5c62c312cda9ad3456316afda5e
haritha <haritharavichandran11@gmail.com>
2026-05-04 00:41:47 +0530

    new changes

--- haritha_assessment_correction/src/test/java/com/test/ProductTest.java
+++ haritha_assessment_correction/src/test/java/com/test/ProductTest.java
@@ -3,23 +3,28 @@ package com.test;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import org.testng.asserts.SoftAssert;
 import com.pages.CartPage;
 import com.pages.LoginPage;
 import com.pages.PaymentPage;
 import com.pages.SearchPage;
 import com.pages.SignupPage;
 import com.pages.UserHomePage;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class ProductTest extends MainTest {
+	private static final Logger log = LogManager.getLogger(ProductTest.class);
   @Test
   public void Search() {
+	  log.info("Starting Search Test");
 	  signup=new SignupPage(driver);
 	  signup.link();
 	  login=new LoginPage(driver);
 	  home=new UserHomePage(driver);
 	  login.waitforlogintext(wait);
+	  log.info("Logging into application");
 	  login.login("haritha27@gmail.com", "haritha@20");
-	  System.out.println( "The user name is:"+ home.loginas(wait));
+	  log.info( "The user name is:"+ home.loginas(wait));
 	  search=new SearchPage(driver);
 	  search.clickProductLink();
 	  search.waitforSearch();
@@ -28,7 +33,7 @@ public class ProductTest extends MainTest {
 	  Assert.assertTrue(search.waitforSearchTest());
 	  Assert.assertTrue(search.isProductDisplayed());
 	  Assert.assertTrue(search.getFirstProductName().toLowerCase().contains("top"));
-	  System.out.println("the searched product item:"+search.getFirstProductName());
+	  log.info("the searched product item:"+search.getFirstProductName());
   }
   @Test(dependsOnMethods="Search")
   public void cart() {
@@ -36,22 +41,27 @@ public class ProductTest extends MainTest {
 	  cart.clickviewProduct();
 	  cart.waitforcart();
 	  Assert.assertEquals(cart.cartsuccessful(), "Your product has been added to cart.");
-	  System.out.println(cart.cartsuccessful());
+	  log.info(cart.cartsuccessful());
 	  cart.clickcontinue();
 	  cart.clickcartlink();
 	  Assert.assertTrue(cart.cartProductText().contains("Blue Top"));
 	  cart.clickcheckout();  
   }
   
+
   @Test(dependsOnMethods="cart")
   public void payment() {
-	  payment = new PaymentPage(driver);
-	  payment.waitforplacebtn();
-	  payment.clickorder();
-	  payment.waitforpaymenttittle();
-	  payment.payment("aaaa", "11111", "311", "09", "2025");
-	  Assert.assertEquals(payment.orderplaced(), "ORDER PLACED!");
-	  System.out.println(payment.orderplaced());
-	  payment.clickcontinue();
+      payment = new PaymentPage(driver);
+      payment.waitforplacebtn();
+      payment.clickorder();
+      payment.waitforpaymenttittle();
+      payment.payment("aaaa", "11111", "311", "09", "2025");
+      SoftAssert soft = new SoftAssert();
+      soft.assertEquals(payment.orderplaced(), "ORDER PLACED!");
+      log.info(payment.orderplaced());
+      payment.clickcontinue();
+      soft.assertAll(); 
   }
+  
+  
 }
--- haritha_assessment_correction/src/test/java/com/test/listenerScreenshot.java
+++ haritha_assessment_correction/src/test/java/com/test/listenerScreenshot.java
@@ -2,21 +2,29 @@ package com.test;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.io.FileHandler;
 
 public class listenerScreenshot {
 
     public static void captureScreenshot(WebDriver driver, String testName) {
+
         TakesScreenshot ts = (TakesScreenshot) driver;
         File src = ts.getScreenshotAs(OutputType.FILE);
+        File folder = new File("screenshots");
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File dest = new File("screenshots/" + testName + "_" + timestamp + ".png");
         try {
-            File dest = new File("screenshots/" + testName + ".png");
             FileHandler.copy(src, dest);
-            System.out.println("Screenshot taken: " + testName);
+            System.out.println("Screenshot saved: " + dest.getPath());
         } catch (IOException e) {
             e.printStackTrace();
         }
